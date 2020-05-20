c85436bafd61e4d9bd708f77695a386fcad79579
Ye Fung Tchen <ye.fung.tchen@klqc.de>
2020-05-20 16:42:41 +0200

    HTML/SCSS: Styling

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,9 @@
 
 
 // Import styles
-import "./styles/style.scss"
-
+import "./styles/body.scss"
+import "./styles/header.scss"
+import "./styles/main.scss"
+import "./styles/footer.scss"
 
 // Export modules
